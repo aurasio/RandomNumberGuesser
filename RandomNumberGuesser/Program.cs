364cc764fd2e72536a6bc27aa11e3062e9b6a873
6030809 <6030809@PT1004-E0432>
2021-10-01 12:16:06 +0100

    Add project files.

---
+++ RandomNumberGuesser/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace randomNumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNum;
+            int guess;
+            int guessAmount=1;
+            bool correct = false;
+            Random r = new Random();
+            randomNum = r.Next(1, 101);
+            Console.WriteLine(randomNum);
+            while (correct == false)
+            {
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("enter guess");
+                        guess = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("that is not a valid integer, try again");
+                    }
+                }
+                while (true);
+                if(randomNum == guess)
+                {
+                    Console.WriteLine("correct! the number was {0}", guess);
+                    Console.WriteLine("guess amount was {0}", guessAmount);
+                    correct = true;
+                }
+                else
+                {
+                    Console.Write("Nope! ");
+                    if(guess > randomNum)
+                    {
+                        Console.Write("too high!");
+                    }
+                    else 
+                    {
+                        Console.Write("too low!");
+                    }
+                    Console.WriteLine(" try again!");
+                    guessAmount++;
+                }
+            }
+        }
+    }
+}
